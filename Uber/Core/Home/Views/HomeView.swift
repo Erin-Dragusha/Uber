6930345ca821e526da51551136c5d3d52a5817db
Erin Dragusha <dragushaerin@gmail.com>
2023-04-28 21:24:09 -0700

    add location search ui

---
+++ Uber/Core/Home/Views/HomeView.swift
@@ -0,0 +1,42 @@
+//
+//  Home.swift
+//  Uber
+//
+//  Created by Erin Dragusha on 2023-04-28.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State private var showLocationSearchView = false
+    
+    var body: some View {
+        ZStack(alignment: .top) {
+            UberMapViewRepresentable()
+                .ignoresSafeArea()
+            
+            if showLocationSearchView {
+               LocationSearchView()
+            }
+            else {
+                LocationSearchActivationView()
+                    .padding(.top, 72)
+                    .onTapGesture {
+                        withAnimation(.spring()) {
+                            showLocationSearchView.toggle()
+                        }
+                    }
+            }
+            
+            MapViewActionButton(showLocationSearchView: $showLocationSearchView)
+                .padding(.leading)
+                .padding(.top, 4)
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
