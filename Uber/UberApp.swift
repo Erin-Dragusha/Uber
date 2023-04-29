79e08493f7f44e91c06e4171752dc5530c2778af
Erin Dragusha <dragushaerin@gmail.com>
2023-04-28 20:03:36 -0700

    Initial Commit

---
+++ Uber/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Erin Dragusha on 2023-04-28.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
