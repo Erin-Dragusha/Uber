0316a5c706da5b994d6c2f674132ca7d6159d6e8
Erin Dragusha <dragushaerin@gmail.com>
2023-04-30 19:33:00 -0700

    add MapView state management

---
+++ Uber/Models/MapViewState.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewState.swift
+//  Uber
+//
+//  Created by Erin Dragusha on 2023-04-30.
+//
+
+import Foundation
+
+enum MapViewState {
+    case noInput
+    case searchingForLocation
+    case locationSelected
+}
